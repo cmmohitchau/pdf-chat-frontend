1e684faf51fc0a12d51f94ea7fede9c12daf2734
cmmohitchau <cmmohitchau@gmail.com>
2026-05-18 21:50:02 +0530

    added more types for session and jwt

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,12 +1,15 @@
-import NextAuth, { DefaultSession } from "next-auth"
+// types/next-auth.d.ts
+import "next-auth"
 
 declare module "next-auth" {
   interface Session {
     accessToken?: string
-
     user: {
       id?: string
-    } & DefaultSession["user"]
+      name?: string | null
+      email?: string | null
+      image?: string | null
+    }
   }
 }
 
